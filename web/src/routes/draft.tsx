3a4239f37724935e58045bfc1c2ca3b51bb36f2e
Sean White <smw0616@gmail.com>
2023-08-22 22:32:54 -0700

    Handle keepers

--- web/src/routes/draft.tsx
+++ web/src/routes/draft.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Rankings } from "../components/Rankings";
-import { getSleeperDraftPicks } from "../util/sleeper";
+import { getSleeperDraftPicks, getSleeperKeepers, getSleeperLeagueId } from "../util/sleeper";
 import { getAllUnderdogPlayers } from "../util/underdog";
 import { Player, getPlayerId, Position } from "../util/player";
 import { SelectionRadio } from "../components/SelectionRadio";
@@ -15,24 +15,28 @@ export function Draft() {
   );
 
   useEffect(() => {
-    function updatePicks() {
-        getSleeperDraftPicks(draftId)
-        .then((players: Player[]) => {
-            const picks = new Set<string>();
-            for (const player of players) {
-            picks.add(getPlayerId(player));
-            }
-            setPicks(picks);
-        })
-        .catch((e) => {
-            console.log(e);
-            throw new Error("Failed to update picks.");
-        });
+    const leagueId = getSleeperLeagueId(draftId);
+    const keeperIds = leagueId
+      .then(async (leagueId) => {
+        if (leagueId === null) return [];
+        const keepers = await getSleeperKeepers(leagueId);
+        return keepers.map((player) => getPlayerId(player));
+      })
+      .catch((e) => {
+        console.log(e);
+        return [] as string[]
+      });
+
+    async function updatePicks() {
+        const players = await getSleeperDraftPicks(draftId)
+        const playerIds = players.map((player) => getPlayerId(player));
+        const picks = new Set<string>(playerIds.concat(await keeperIds));
+        setPicks(picks);
     }
 
     getAllUnderdogPlayers(setPlayers);
-    updatePicks();
-    setInterval(updatePicks, 1000);
+    updatePicks().catch(console.error);
+    setInterval(() => {updatePicks().catch(console.error)}, 1000);
   }, [draftId]);
 
   return (
--- web/src/util/sleeper.tsx
+++ web/src/util/sleeper.tsx
@@ -10,6 +10,19 @@ interface PlayerResponseObject {
   }
 }
 
+interface SleeperPlayer {
+  first_name: string;
+  last_name: string;
+  position: string;
+}
+
+const sleeperPlayers = fetch(SLEEPER_API + "/players/nfl").then((response) => {
+  if (!response.ok) throw new Error("Failed to fetch sleeper players.");
+  return response.json().then((responseJson) => {
+    return responseJson as {[key: string]: SleeperPlayer};
+  })
+});
+
 export async function getSleeperDraftPicks(draftId: string): Promise<Player[]> {
   if (draftId === "") return [];
 
@@ -24,4 +37,45 @@ export async function getSleeperDraftPicks(draftId: string): Promise<Player[]> {
       position: p.metadata.position,
     }
   })
+}
+
+export async function getSleeperLeagueId(draftId: string): Promise<string> {
+  const response = await fetch(SLEEPER_API + `/draft/${draftId}`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch draft info for draftId ${draftId}`);
+  }
+  return response.json().then((draft: {league_id: string}) => {
+    return draft.league_id;
+  })
+}
+
+export async function getSleeperKeepers(leagueId: string): Promise<Player[]> {
+  const response = await fetch(SLEEPER_API + `/league/${leagueId}/rosters`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch league rosters for leagueId ${leagueId}`);
+  }
+  const keeperIds: string[] = await response.json().then((rosters: {keepers: string[]}[]) => {
+    let ids: string[] = [];
+    for (const roster of rosters) {
+      if (roster.keepers === null) continue;
+      ids = ids.concat(roster.keepers);
+    }
+    return ids;
+  })
+
+  return Promise.all(keeperIds.map((id) => {
+    return getSleeperPlayer(id);
+  }));
+}
+
+async function getSleeperPlayer(playerId: string): Promise<Player> {
+  const sleeperPlayer = (await sleeperPlayers)[playerId];
+  if (sleeperPlayer == undefined) {
+    throw new Error(`Player with sleeper id ${playerId} does not exist.`);
+  }
+  return {
+    firstName: sleeperPlayer.first_name,
+    lastName: sleeperPlayer.last_name,
+    position: sleeperPlayer.position,
+  };
 }
