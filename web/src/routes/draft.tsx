d27e1c3aebce7005c9ef2cd00ce617dc8325395c
Sean White <smw0616@gmail.com>
2023-08-23 16:25:21 -0700

    Add back button to draft page

--- web/src/routes/draft.tsx
+++ web/src/routes/draft.tsx
@@ -5,6 +5,7 @@ import { getAllUnderdogPlayers } from "../util/underdog";
 import { Player, getPlayerId, Position } from "../util/player";
 import { SelectionRadio } from "../components/SelectionRadio";
 import { useLoaderData, useNavigate } from "react-router-dom";
+import './draft.css';
   
 export function Draft() {
   const navigate = useNavigate();
@@ -32,33 +33,42 @@ export function Draft() {
       })
       .catch((e) => {
         console.log(e);
-        return [] as string[]
+        return [] as string[];
       });
 
     async function updatePicks() {
-        const players = await getSleeperDraftPicks(draftId)
-        const playerIds = players.map((player) => getPlayerId(player));
-        const picks = new Set<string>(playerIds.concat(await keeperIds));
-        setPicks(picks);
+      const players = await getSleeperDraftPicks(draftId);
+      const playerIds = players.map((player) => getPlayerId(player));
+      const picks = new Set<string>(playerIds.concat(await keeperIds));
+      setPicks(picks);
     }
 
     getAllUnderdogPlayers(setPlayers);
     updatePicks().catch(console.error);
     const updateTimeout = setInterval(() => {
-      updatePicks().catch(console.error)
+      updatePicks().catch(console.error);
     }, 1000);
 
     return () => {
       if (updateTimeout) {
         clearTimeout(updateTimeout);
       }
-    }
+    };
   }, [draftId, navigate]);
 
   return (
-    <>
-      <SelectionRadio setRankingSelection={setRankingSelection} />
-      <Rankings position={rankingSelection} picks={picks} players={players} />
-    </>
+    <div className="page">
+      <div className="nav">
+        <button className="back-button" onClick={() => navigate("/")}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 20 20" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M19 12H6M12 5l-7 7 7 7"/>
+          </svg>
+        </button>
+      </div>
+      <div className="content">
+        <SelectionRadio setRankingSelection={setRankingSelection} />
+        <Rankings position={rankingSelection} picks={picks} players={players} />
+      </div>
+    </div>
   );
 }
